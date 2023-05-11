f69795df6c39ed415bd977908754416455f2537e
Shamanesss <mentulbenodorna@gmail.com>
2023-05-11 22:30:52 +0200

    spl

--- src/weather_repository_sqlite.py
+++ src/weather_repository_sqlite.py
@@ -1,22 +1,29 @@
 import sqlite3
-con=sqlite3.content("WEATHER.db")
-cur=con.cursor()
-cur.execute ("CREATE TABLE cities(id,name,temperature,rain_probability)")
+con = sqlite3.content("WEATHER.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE cities(id,name,temperature,rain_probability)")
+con.commit()
 
 
+def create(weather):
+    cur.execute("INSERT INTO cities (id, name, temperature, rain_probability) VALUES (?, ?, ?, ?)",
+                (weather['id'], weather['name'], weather['temperature'], weather['rain_probability']))
+    con.commit()
 
-def create (weather):
-    WEATHER_DATA[weather['id']]=weather
 
-def read (weather):
-    return WEATHER_DATA.get(id)
+def update(id, temperature, rain_probability):
+    cur.execute("UPDATE cities SET temperature = ?, rain_probability = ? WHERE id = ?",
+                (temperature, rain_probability, id))
+    con.commit()
 
-def read ():
+
+def read():
     res = cur.execute("SELECT name FROM cities")
     resultado = res.fetchone
     print(resultado)
     return resultado
 
 
-def delete (id):
-    del WEATHER_DATA [id]
+def delete(id):
+    cur.execute("DELETE FROM cities WHERE id = ?", (id,))
+    con.commit()
