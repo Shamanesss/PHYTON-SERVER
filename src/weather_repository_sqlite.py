d91d47c56e9fca21602c5cb8309c4d745a449956
Shamanesss <mentulbenodorna@gmail.com>
2023-05-14 12:12:36 +0200

    sin flask

--- src/weather_repository_sqlite.py
+++ src/weather_repository_sqlite.py
@@ -1,29 +1,53 @@
 import sqlite3
-con = sqlite3.content("WEATHER.db")
+
+con = sqlite3.connect("WEATHER.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE cities(id,name,temperature,rain_probability)")
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS cities(
+                id TEXT PRIMARY KEY,
+                name TEXT,
+                temperature NUMERIC,
+                rain_probability real)""")
+
+ciudades =[
+    ("BAl","Bali", 18, 0.9),
+    ("Can","canarias", 20, 0.3),
+    
+]
+
+# def create(weather):
+cur.executemany("INSERT INTO cities (id, name, temperature, rain_probability) VALUES (?, ?, ?, ?)", ciudades)
+    # cur.execute("INSERT INTO cities (id, name, temperature, rain_probability) VALUES (?, ?, ?, ?)",
+    #             (weather['id'], weather['name'], weather['temperature'], weather['rain_probability']))
+
+
+
+# def update(id, temperature, rain_probability):
+#     cur.execute("UPDATE cities SET temperature = ?, rain_probability = ? WHERE id = ?",
+#                 (temperature, rain_probability, id))
+#     con.commit()
+cur.execute("UPDATE cities SET name='Austria' Where id='AUS'")
+
+# def read():
+cur.execute("SELECT * FROM cities")
+ciudades = cur.fetchall()
+for ciudad in ciudades:
+    print(ciudad)
+
+    # res= cur.execute("SELECT name FROM cities")
+    # resultado = res.fetchone
+    # print(resultado)
+    # return resultado
+
+
+# def delete(id):
+#     cur.execute("DELETE FROM cities WHERE id = ?", (id,))
+#     con.commit()
+
+cur.execute("SELECT * FROM cities WHERE name = 'Bilbao'")
+ciudades=cur.fetchall()
+print(ciudades)
+
+
 con.commit()
-
-
-def create(weather):
-    cur.execute("INSERT INTO cities (id, name, temperature, rain_probability) VALUES (?, ?, ?, ?)",
-                (weather['id'], weather['name'], weather['temperature'], weather['rain_probability']))
-    con.commit()
-
-
-def update(id, temperature, rain_probability):
-    cur.execute("UPDATE cities SET temperature = ?, rain_probability = ? WHERE id = ?",
-                (temperature, rain_probability, id))
-    con.commit()
-
-
-def read():
-    res = cur.execute("SELECT name FROM cities")
-    resultado = res.fetchone
-    print(resultado)
-    return resultado
-
-
-def delete(id):
-    cur.execute("DELETE FROM cities WHERE id = ?", (id,))
-    con.commit()
+con.close()
